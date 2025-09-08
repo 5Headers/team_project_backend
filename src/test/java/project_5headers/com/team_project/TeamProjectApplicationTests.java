de09ad10e9213868a3eb4c581399ebfbdb4cea1f
hdni2002 <hdni2003@gmail.com>
2025-09-08 20:14:29 +0900

    Init: commit

---
+++ src/test/java/project_5headers/com/team_project/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project_5headers.com.team_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
