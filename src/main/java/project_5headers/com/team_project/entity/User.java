f7392f7597fd107de5c757f7264d802f102a8b2d
JINHYUN-BRIGHTEVERYDAY <jinibrighteveryday@gmail.com>
2025-09-08 21:25:55 +0900

    feat: 백엔드 기본 골격 작성

---
+++ src/main/java/project_5headers/com/team_project/entity/User.java
@@ -0,0 +1,35 @@
+package project_5headers.com.team_project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+}
