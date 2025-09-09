ee939e9256c997f148bc0237ee3af203844ea617
JINHYUN-BRIGHTEVERYDAY <jinibrighteveryday@gmail.com>
2025-09-09 21:01:14 +0900

    feat: security 수정

--- src/main/java/project_5headers/com/team_project/dto/LoginRequestDto.java
+++ src/main/java/project_5headers/com/team_project/dto/LoginRequestDto.java
@@ -1,4 +1,14 @@
 package project_5headers.com.team_project.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequestDto {
+
+    private String username;
+    private String password;
+
 }
---
+++ src/main/java/project_5headers/com/team_project/sequrity/PrincipalDetails.java
@@ -0,0 +1,51 @@
+package project_5headers.com.team_project.sequrity;
+
+
+import project_5headers.com.team_project.entity.User;
+import java.util.Collection;
+import java.util.Collections;
+
+public class PrincipalDetails implements UserDetails {
+
+    private final User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<String> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+
+    }
+
+}
