e010fd5a139dcf3acddc15e2aa3aed573c2722f4
hdni2002 <hdni2003@gmail.com>
2025-09-09 20:57:25 +0900

    Feat:commit

---
+++ src/main/java/project_5headers/com/team_project/entity/Estimate_part.java
@@ -0,0 +1,20 @@
+package project_5headers.com.team_project.entity;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Estimate_part {
+    private Integer estimatePartId;
+    private Integer estimateId;
+    private String category;
+    private String name;
+    private Integer price;
+    private String link;
+    private String storeType;
+    private LocalDateTime createdAt;
+}
