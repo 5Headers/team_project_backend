0184cf18dfc34bb3d9b88795bcce9822545e9357
JINHYUN-BRIGHTEVERYDAY <jinibrighteveryday@gmail.com>
2025-09-09 19:26:30 +0900

    ããdto 실험적 추가

--- src/main/java/project_5headers/com/team_project/dto/UserRequestDto.java
+++ src/main/java/project_5headers/com/team_project/dto/UserRequestDto.java
@@ -1,4 +1,14 @@
 package project_5headers.com.team_project.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRequestDto {
+
+    private String username;
+    private String password;
+    private String email;
 }
