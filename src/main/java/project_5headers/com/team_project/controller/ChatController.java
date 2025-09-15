30ce0767ce9008be307ac8f7eafacc75a120655a
hdni2002 <hdni2003@gmail.com>
2025-09-16 05:47:26 +0900

    Feat:commit

---
+++ src/main/java/project_5headers/com/team_project/controller/ChatController.java
@@ -0,0 +1,22 @@
+package project_5headers.com.team_project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project_5headers.com.team_project.dto.ApiRespDto;
+import project_5headers.com.team_project.service.ChatGPTService;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    @Autowired
+    private ChatGPTService chatGPTService;
+
+    @PostMapping("/estimate")
+    public ResponseEntity<?> getEstimate(@RequestParam String cost, @RequestParam String purpose) {
+        String prompt = "사용자의 컴퓨터 용도: " + purpose + ", 예산: " + cost + "원에 맞는 추천 견적을 알려줘.";
+        String result = chatGPTService.getChatGPTResponse(prompt);
+        return ResponseEntity.ok(new ApiRespDto<>("success", "견적 생성 완료", result));
+    }
+}
