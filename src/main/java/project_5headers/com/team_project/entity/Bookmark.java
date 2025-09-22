4d0c3b5e2e4329a63766c99c9136d1527fb6939e
Juesonghyeon <199971alfks@gmail.com>
2025-09-22 21:42:22 +0900

    Feat: 수정 시도

--- src/main/java/project_5headers/com/team_project/entity/Bookmark.java
+++ src/main/java/project_5headers/com/team_project/entity/Bookmark.java
@@ -1,15 +1,21 @@
 package project_5headers.com.team_project.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bookmark {
-        private Integer bookmarkId;
-        private Integer estimateId;
-        private Integer userId;
-        private LocalDateTime createDt;
+    private Integer bookmarkId;
+    private Integer estimateId;
+    private Integer userId;
+    private String title;
+    private String content;
+    private LocalDateTime createDt;
 }
