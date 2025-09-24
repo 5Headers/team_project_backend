112750ad64191e6d2dd5863415e8ec480d922756
Kwak-hojun <hojun6901@gmail.com>
2025-09-24 19:41:14 +0900

    Feat:GPT DB 마무리

--- src/main/java/project_5headers/com/team_project/entity/EstimatePart.java
+++ src/main/java/project_5headers/com/team_project/entity/EstimatePart.java
@@ -1,6 +1,5 @@
 package project_5headers.com.team_project.entity;
 
-
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,12 +8,12 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class EstimatePart {
-    private Integer estimatePartId;
-    private Integer estimateId;
-    private String category;
-    private String name;
-    private Integer price;
-    private String link;
-    private String storeType;
+    private Integer estimatePartId; // PK
+    private Integer estimateId;     // FK
+    private String category;        // CPU, GPU, RAM ...
+    private String name;            // 제품명
+    private Integer price;          // 가격
+    private String link;            // 최저가 링크
+    private String storeType;       // ONLINE / OFFLINE 구분
     private LocalDateTime createdAt;
 }
