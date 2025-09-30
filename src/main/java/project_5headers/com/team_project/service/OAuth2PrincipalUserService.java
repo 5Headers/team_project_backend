c555617c91f0798a13227d3afbdec6129c21a217
Kwak-hojun <hojun6901@gmail.com>
2025-09-30 20:49:52 +0900

    Faet:찜성공

--- src/main/java/project_5headers/com/team_project/service/OAuth2PrincipalUserService.java
+++ src/main/java/project_5headers/com/team_project/service/OAuth2PrincipalUserService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-
+//test
 @Service
 public class OAuth2PrincipalUserService extends DefaultOAuth2UserService {
 
